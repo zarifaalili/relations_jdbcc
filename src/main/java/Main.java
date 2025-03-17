2da7363674623960a37943a6cfc076e3af7122dd
zarifaalili <alilizarifa@gmail.com>
2025-03-17 19:21:41 +0400

    commi tmessage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,6 @@ import Config.DatabaseConfig;
 import repo.Repository;
 
 import java.sql.Connection;
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,11 +13,11 @@ public class Main {
 
 //        repository.query_1(connection,2021,2023);
 
-        repository.desc_book(connection);
+//        repository.desc_book(connection);
 //        repository.all_book(connection);
 //repository.insert_book(connection,"Sarka
 // ç",2025,3);
-//repository.insert_author(connection,"Loresima","Kubra");
+repository.insert_author(connection,"Lore","Aysha");
 //repository.insert_author(connection,"Maral","Atmaca");
 //repository.all_authors(connection);
 
