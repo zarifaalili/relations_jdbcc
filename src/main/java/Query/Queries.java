95ebbff4a4d270e7df8f875bbd25a1f0e6a256f0
zarifaalili <alilizarifa@gmail.com>
2025-03-17 19:58:27 +0400

    diger brachin 1ci commiti

--- src/main/java/Query/Queries.java
+++ src/main/java/Query/Queries.java
@@ -9,12 +9,12 @@ public enum Queries {
             ");"),
 
     CREATE_TABLE_BOOK("CREATE TABLE IF NOT EXISTS book (\n" +
-            "    id BIGSERIAL PRIMARY KEY,\n" +  // Kitab ID-si avtomatik artsın
+            "    id BIGSERIAL PRIMARY KEY,\n" +
             "    name VARCHAR(100) NOT NULL,\n" +
             "    year INT NOT NULL,\n" +
             "    author_id INT,\n" +
             "    FOREIGN KEY (author_id) REFERENCES author(id) ON DELETE CASCADE,\n" +
-            "    UNIQUE (name, year, author_id)\n" +  // Eyni müəllifin eyni kitabı 2 dəfə ola bilməz
+            "    UNIQUE (name, year, author_id)\n" +
             ");"),
 
     INSERT_AUTHOR("INSERT INTO author (name, surname) \n" +
@@ -23,15 +23,15 @@ public enum Queries {
 
     FIND_ALL_AUTHORS("SELECT a.id, a.name as author_name, a.surname, b.name as book_name, b.year \n" +
             "FROM author a \n" +
-            " JOIN book b ON a.id = b.author_id;"), // Sol birləşmə ilə müəllifin kitabı yoxdursa da göstər
+            " JOIN book b ON a.id = b.author_id;"),
 
     UPDATE_AUTHOR("UPDATE author SET name=?, surname=? WHERE id=?;"),
 
-    DELETE_AUTHOR("DELETE FROM author WHERE id=?;"), // `ON DELETE CASCADE` aktivdir, kitablar da silinəcək
+    DELETE_AUTHOR("DELETE FROM author WHERE id=?;"),
 
     INSERT_BOOK("INSERT INTO book (name, year, author_id) \n" +
             "VALUES (?, ?, ?) \n" +
-            "ON CONFLICT (name, year, author_id) DO NOTHING;"), // Eyni müəllifin eyni kitabı təkrar olmasın
+            "ON CONFLICT (name, year, author_id) DO NOTHING;"),
 
     FIND_ALL_BOOKS("SELECT b.id, b.name as book_name, b.year, a.name AS author_name, a.surname  \n" +
             "FROM book b \n" +
